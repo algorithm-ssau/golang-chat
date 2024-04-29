58c109f7e719319bf8b7b790b136569f7e7b0460
Екатерина Балашова <rinaleem@mail.ru>
2024-04-29 18:03:27 +0400

    Updated ChatInput.jsx

---
+++ frontend/src/components/ChatInput/ChatInput.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './ChatInput.scss';
+
+class ChatInput extends Component{
+    render(){
+        return(
+            <div className = 'ChatInput'>
+                <input onKeyDown={this.props.send} placeholder = "Введите сообщение"></input>
+            </div>
+        )
+    }
+
+}
