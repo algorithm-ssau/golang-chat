e611ae0cbcc65d20d25429c7e9a95c6f60a84584
Екатерина Балашова <rinaleem@mail.ru>
2024-04-29 17:58:39 +0400

    Updated header.jsx

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => (
+    <div className = 'header'>
+        <h2>Super Golang Chat</h2>
+    </div>
+);
+
+export default Header;
