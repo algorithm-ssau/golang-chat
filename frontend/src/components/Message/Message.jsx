e2e28a8e9b88f1338b4f68a95662af25e85e09e6
Екатерина Балашова <rinaleem@mail.ru>
2024-05-01 11:53:50 +0400

    Updated Message.jsx

--- frontend/src/components/Message/Message.jsx
+++ frontend/src/components/Message/Message.jsx
@@ -1,2 +1,9 @@
 import React, {Component} from 'react';
 import './Message.scss';
+
+class Message extends Component{
+    constructor(props){
+        super(props);
+        let tmp= JSON.parse(this.props.message);
+    }
+}
