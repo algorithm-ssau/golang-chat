54e5241fd16f472319b742f1bdd2dbb8289a5cd1
PurreCoder <113393847+PurreCoder@users.noreply.github.com>
2024-05-18 11:30:18 +0400

    Update client.py

--- backend/client.py
+++ backend/client.py
@@ -3,6 +3,22 @@ import select
 import sys
 
 
+def get_logger(name=__file__, file='log.txt', encoding='utf-8'):
+    log = logging.getLogger(name)
+    log.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('[%(asctime)s] %(filename)s:%(lineno)d %(levelname)-8s %(message)s')
+
+    fh = logging.FileHandler(file, encoding=encoding)
+    fh.setFormatter(formatter)
+    log.addHandler(fh)
+
+    sh = logging.StreamHandler(stream=sys.stdout)
+    sh.setFormatter(formatter)
+    log.addHandler(sh)
+
+    return log
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 if len(sys.argv) != 3:
     print("Correct usage: script, IP address, port number")
@@ -20,10 +36,12 @@ while True:
     for socks in read_sockets:
         if socks == server:
             message = socks.recv(2048)
+            logger.info(message)
             print(message)
         else:
             message = sys.stdin.readline()
             server.send(message)
+            logger.info(message)
             sys.stdout.write("<You>")
             sys.stdout.write(message)
             sys.stdout.flush()
